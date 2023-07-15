dfac3d47a45d857a26a5b3431840589db6f33e87
Allyson Gonzales <Allysonmg21@gmail.com>
2023-07-15 14:11:17 -0700

    Edited Nav component for hamburger menu

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   function showNavigation() {
     return (
-      <ul className="flex-row">
+      <ul className={`flex-row ${isMenuOpen ? "slide-in" : "slide-out"}`}>
         <li className="mx-1">
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
         </li>
         <li className="mx-1">
-          <Link to="/">Calendar</Link>
+          <Link to="/" onClick={closeMenu}>
+            Calendar
+          </Link>
         </li>
         <li className="mx-1">
-          <Link to="/">Profile</Link>
+          <Link to="/" onClick={closeMenu}>
+            Profile
+          </Link>
         </li>
         <li className="mx-1">
-          <a href="/">Logout</a>
+          <Link to="/" onClick={closeMenu}>
+            Logout
+          </Link>
         </li>
       </ul>
     );
   }
-  return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span role="img" aria-label=""></span>
-          FitTracker
-        </Link>
-      </h1>
 
-      <nav className="navbar">{showNavigation()}</nav>
-    </header>
+  return (
+    <div className="container">
+      <header className="flex-row px-1">
+        <nav className="navbar">
+          <Link to="/">
+            <img src={"FitLogo.png"} alt="FitTracker Logo" className="logo" />
+          </Link>
+  
+          <div
+            className={`hamburger ${isMenuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </nav>
+  
+        {showNavigation()}
+      </header>
+    </div>
   );
 }
+
 export default Nav;
