bd121c31baeeeaea0974174841704da4a8beed4c
Allyson Gonzales <Allysonmg21@gmail.com>
2023-10-24 12:33:00 -0700

    moved <h2> above images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,12 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='containerImg'>
-      <img src='lifting.jpg' alt='Lifting' className='image' />
       <h2 className='heading'>Lifting</h2>
-      <img src='Calendar.jpg'alt=''className='image'/>
+      <img src='lifting.jpg' alt='Lifting' className='image' />
       <h2 className='heading'>Calendar</h2>
-      <img src='Yoga.jpg' alt='Yoga' className='image' />
+      <img src='Calendar.jpg'alt=''className='image'/>
       <h2 className='heading'>Yoga</h2>
+      <img src='Yoga.jpg' alt='Yoga' className='image' />
     </div>
   );
 };
