88db53a38d835e93490d7b3181667732fa9ecb3f
Allyson Gonzales <Allysonmg21@gmail.com>
2023-10-24 12:32:33 -0700

    Improved footer and placed text into an <li>

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React from "react";
 
-function Footer () { 
-    return (
-        <footer className='footer'>
-            <div className='container'>
-                <p> &copy;
-                </p>
-                
-            </div>
-        </footer>
-    );
- }
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>
+          <ul>
+            <li>Contact Us</li>
+            <li>About Us</li>
+            <li>Terms of Use</li>
+          </ul>
+        </p>
+      </div>
+      <div className="container">
+        <p>
+          <ul>
+            <li>Facebook </li>
+            <li>Twitter</li>
+            <li>Youtube</li>
+          </ul>
+        </p>
+      </div>
+      <div className="copyright">
+        &copy;2023 DevDuo, Seattle, Washington All Rights Reserved
+      </div>
+    </footer>
+  );
+}
 
- export default Footer;
+export default Footer;
