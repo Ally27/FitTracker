1f37c3ebc6fa3a92db4c4bbc570639a25cd5decc
Allyson Gonzales <Allysonmg21@gmail.com>
2023-10-24 00:07:44 -0700

    Organizing code by: <img,<h2> into div

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
-// import { useQuery } from '@apollo/client';
-
-const Home = () =>{
-    return(
-        <div className='container'>
-        </div>
-    );
+const Home = () => {
+  return (
+    <div className='containerImg'>
+      <img src='lifting.jpg' alt='Lifting' className='image' />
+      <h2 className='heading'>Lifting</h2>
+      <img src='Calendar.jpg'alt=''className='image'/>
+      <h2 className='heading'>Calendar</h2>
+      <img src='Yoga.jpg' alt='Yoga' className='image' />
+      <h2 className='heading'>Yoga</h2>
+    </div>
+  );
 };
 
 export default Home;
