89f1bda8df61d28e2d1e1a5499aba09dbd1c48f6
Allyson Gonzales <Allysonmg21@gmail.com>
2023-10-24 00:02:54 -0700

    Created base footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer () { 
+    return (
+        <footer className='footer'>
+            <div className='container'>
+                <p> &copy;
+                </p>
+                
+            </div>
+        </footer>
+    );
+ }
+
+ export default Footer;
